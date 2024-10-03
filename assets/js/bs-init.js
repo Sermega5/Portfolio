b58c6abc8c3fc2c7ff913b7e269a0d7397de7dc9
Sermega <ser.matossi@gmail.com>
2024-10-03 14:20:38 +0200

    Actualizado el formlario de envio
    se le a quitado el css para ponerle estilos de css y retirado el javacript sustiuido por bootsrap

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -1,16 +1,40 @@
-
 if (window.innerWidth < 768) {
-	[].slice.call(document.querySelectorAll('[data-bss-disabled-mobile]')).forEach(function (elem) {
-		elem.classList.remove('animated');
-		elem.removeAttribute('data-bss-hover-animate');
-		elem.removeAttribute('data-aos');
-		elem.removeAttribute('data-bss-parallax-bg');
-		elem.removeAttribute('data-bss-scroll-zoom');
-	});
+  [].slice
+    .call(document.querySelectorAll("[data-bss-disabled-mobile]"))
+    .forEach(function (elem) {
+      elem.classList.remove("animated");
+      elem.removeAttribute("data-bss-hover-animate");
+      elem.removeAttribute("data-aos");
+      elem.removeAttribute("data-bss-parallax-bg");
+      elem.removeAttribute("data-bss-scroll-zoom");
+    });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-	if ('AOS' in window) {
-		AOS.init();
-	}
-}, false);
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    if ("AOS" in window) {
+      AOS.init();
+    }
+  },
+  false
+);
+
+(function () {
+  "use strict";
+  const forms = document.querySelectorAll(".needs-validation");
+
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
