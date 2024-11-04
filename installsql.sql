64fc85f961be9a8f8517eb7cf0f331884dd64b79
Sermega <ser.matossi@gmail.com>
2024-11-04 12:22:59 +0100

    Update installsql.sql

--- installsql.sql
+++ installsql.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS `proyectos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(20) NOT NULL,
   `image` varchar(100) NOT NULL,
-  `description` varchar(120) NOT NULL,
+  `description` varchar(120) NOT NULL
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
